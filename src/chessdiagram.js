bb0314375e57419e35b2cc1c0abb85cc85f82569
leotrubach <leotrubach@gmail.com>
2018-11-05 04:38:39 +0600

    Add `last` option for startMove

--- src/chessdiagram.js
+++ src/chessdiagram.js
@@ -80,6 +80,9 @@ class Chessdiagram extends Component {
 
 	// returns halfmove count of the prop startMove ////
 	get startMove() {
+	  if (this.props.startMove === 'last') {
+	    return this.moves.length - 1;
+    }
 		let currentMove;
 		if (typeof this.props.startMove === 'number' || parseInt(this.props.startMove)) {
 			// halfMove provided
